e5d277d6797b7a0da7ad7a6ce21843017b557e5d
Sara <sarasanchezcamilleri@gmail.com>
2025-04-28 12:13:43 +0200

    feat: creación de modelos y seeders iniciales para entidades principales

--- refugio/database/migrations/0001_01_01_000000_create_users_table.php
+++ refugio/database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone')->nullable(); // Añadir phone
+            $table->string('address')->nullable(); // Añadir address
             $table->rememberToken();
             $table->timestamps();
         });
