e1a1c80b60c3b88fec13ed6151207bac9cb855bb
Sara <sarasanchezcamilleri@gmail.com>
2025-05-05 12:48:20 +0200

    Creación de .env.wailway para conexión con base de datos online

--- refugio/database/seeders/DatabaseSeeder.php
+++ refugio/database/seeders/DatabaseSeeder.php
@@ -2,17 +2,6 @@
 
 namespace Database\Seeders;
 
-use Database\Seeders\UserSeeder;
-use Database\Seeders\AnimalSeeder;
-use Database\Seeders\AdoptionSeeder;
-use Database\Seeders\FosterSeeder;
-use Database\Seeders\VeterinaryHistorySeeder;
-use Database\Seeders\AnimalMedicationSeeder;
-use Database\Seeders\AdoptionRequestSeeder;
-use Database\Seeders\FosterRequestSeeder;
-use Database\Seeders\VolunteerRequestSeeder;
-use Database\Seeders\ContactMessageSeeder;
-
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -35,5 +24,4 @@ class DatabaseSeeder extends Seeder
             ContactMessageSeeder::class,
         ]);
     }
-    
 }
