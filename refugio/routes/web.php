46db64bd577974b628e3bc754c992192b16a5d1e
Sara <sarasanchezcamilleri@gmail.com>
2025-05-22 15:34:46 +0200

    Reestructuración de Http\Controllers

--- refugio/routes/web.php
+++ refugio/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\UserController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Grupo de rutas protegidas para administración
+Route::prefix('admin')->middleware(['auth', 'is_admin'])->name('admin.')->group(function () {
+    Route::resource('users', UserController::class)->names('users');
 });
