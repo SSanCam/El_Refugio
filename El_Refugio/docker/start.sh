576994090b9cd886491fc8b7c94b31db1cec0246
Sara <sarasanchezcamilleri@gmail.com>
2025-12-11 19:36:19 +0100

    Fix: Dockerfile

--- El_Refugio/docker/start.sh
+++ El_Refugio/docker/start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+if ! grep -q "APP_KEY=" .env 2>/dev/null; then
+    echo "APP_KEY=${APP_KEY}" >> .env
+fi
+
 echo "Ejecutando migraciones y seeders..."
 php artisan migrate --force --seed
 
