3fa099e32cdd3e4205808f5a61fab51e9fe603e9
Sara <sarasanchezcamilleri@gmail.com>
2025-12-13 00:34:40 +0100

    Agregado Tailwind a .css

--- El_Refugio/resources/views/layouts/public.blade.php
+++ El_Refugio/resources/views/layouts/public.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     {{--  --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
--- El_Refugio/vite.config.js
+++ El_Refugio/vite.config.js
@@ -1,16 +1,14 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: false,
-        }),
-    ],
-    build: {
-        manifest: true,
-        outDir: 'public/build',
-        emptyOutDir: true,
-    },
-});
+  plugins: [
+    laravel({
+      input: [
+        'resources/css/app.css',
+        'resources/js/app.js',
+      ],
+      refresh: false,
+    }),
+  ],
+})
