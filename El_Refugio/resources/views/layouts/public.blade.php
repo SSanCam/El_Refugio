e2fa255e010538590dae69e8bcc46f52d5d0df27
Sara <sarasanchezcamilleri@gmail.com>
2025-12-13 00:05:56 +0100

    Render test

--- El_Refugio/resources/views/layouts/public.blade.php
+++ El_Refugio/resources/views/layouts/public.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     {{--  --}}
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
     <title>@yield('title', 'El Refugio | Adopción y acogida de animales')</title>
 
