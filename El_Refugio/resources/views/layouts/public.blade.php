86a4a540e48d0db815cf974aa8c5dca08e16d09b
Sara <sarasanchezcamilleri@gmail.com>
2025-12-12 21:10:09 +0100

    fix: regenerado build de Vite y activado @vite en layout público

--- El_Refugio/resources/views/layouts/public.blade.php
+++ El_Refugio/resources/views/layouts/public.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    {{--  @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
    
 
     <title>@yield('title', 'El Refugio | Adopción y acogida de animales')</title>
